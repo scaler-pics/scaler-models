d88d08f5f46ece0e599efd96f1b1f7605d764a8d
Matej Ukmar <matejukmar@gmail.com>
2024-01-16 07:53:39 +0000

    Updated auth

--- auth.ts
+++ auth.ts
@@ -26,8 +26,10 @@ export interface EmailSignupRequest {
 	email: string;
 	name: string;
 	password: string;
+	plan: string;
 }
 
-export interface SignupWithTokenRequest {
+export interface SignupWithCodeRequest {
 	token: string;
+	code: string;
 }
