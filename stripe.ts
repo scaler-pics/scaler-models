4da6894ce259ad006c0f222767b0126b8e2eaf43
Matej Ukmar <matejukmar@gmail.com>
2024-02-08 15:44:21 +0000

    Added subscription info models

--- stripe.ts
+++ stripe.ts
@@ -16,3 +16,26 @@ export interface Subscription {
 export interface PortalSessionRequest {
 	stripeCustomerId: string;
 }
+
+export type CustomerSubscriptionState =
+	| 'noCustomer'
+	| 'noSubscription'
+	| 'hasSubscription';
+
+export interface SubscriptionInfoResponse {
+	state: CustomerSubscriptionState;
+	subscription?: SubscriptionInfo;
+}
+
+export interface SubscriptionInfo {
+	id: string;
+	status: string;
+	cancel_at_period_end: boolean;
+	current_period_end: number;
+	current_period_start: number;
+	start_date: number;
+	trial_start: number | null;
+	trial_end: number | null;
+	plan: string | null;
+	product: string | null;
+}
