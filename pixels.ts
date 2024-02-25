de280ed8f669bc294ddda697e0506dfd30c9efc4
Matej Ukmar <matejukmar@gmail.com>
2024-02-25 13:42:28 +0000

    Updated usage model

--- pixels.ts
+++ pixels.ts
@@ -1,4 +1,26 @@
-export interface AddPixelsRequest {
-	userId: number;
+import { DestinationImageType, SourceImageType } from './transform';
+
+export interface AddUsageRequest {
+	userId: string;
 	pixels: number;
+	date: string;
+	source: SourceInfo;
+	destination: DestinationInfo[];
+}
+
+export interface SourceInfo {
+	type: SourceImageType;
+	sizeBytes: number;
+	sizePixels: Size;
+}
+
+export interface DestinationInfo {
+	width: number;
+	height: number;
+	type: DestinationImageType;
+}
+
+export interface Size {
+	width: number;
+	height: number;
 }
